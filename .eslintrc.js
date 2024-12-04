8f20cad4fd2308088fad7207975fbc4617e892a4
Raveesha-Induwara <raveeshe.abeysinghe@gmail.com>
2024-12-04 23:00:37 +0530

    Add custom fonts

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,12 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    'react-hooks/exhaustive-deps': 'error',
+    'prettier/prettier': ['error', {endOfLine: 'auto'}],
+  },
 };
